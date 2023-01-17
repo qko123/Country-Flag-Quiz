334555a213626a304bd9281600415ec6a22f58e2
Quinn Kozak <quinn.kozak@icloud.com>
2023-01-17 09:46:15 -0600

    Added all original countries

--- Country Flag Quiz/Models/Data.swift
+++ Country Flag Quiz/Models/Data.swift
@@ -10,7 +10,7 @@ struct Data {
 
     let questions = [
 
- 
+ // Angola, Austria, China, England, Estonia, France, Germany, Greece, India, Israel, Italy, Norway, Pakistan, Peru, Saudi Arabia, South Africa, Spain, Sweden, Ukraine, United States //
 
         Question(correctAnswer:
 
@@ -78,10 +78,280 @@ struct Data {
                 Answer(text: "Israel", isCorrect: false)
 
            ])
+        
+        ,
+        
+        
+        Question(correctAnswer:
 
+                Answer(text: "Angola", isCorrect: true),
 
+                 incorrectAnswer: [
+
+                Answer(text: "Austria", isCorrect: false),
+
+                Answer(text: "Estonia", isCorrect: false),
+
+                Answer(text: "India", isCorrect: false)
+
+           ])
+        
+        ,
+        
+        
+        Question(correctAnswer:
+
+                Answer(text: "Austria", isCorrect: true),
+
+                 incorrectAnswer: [
+
+                Answer(text: "Peru", isCorrect: false),
+
+                Answer(text: "France", isCorrect: false),
+
+                Answer(text: "Saudi Arabia", isCorrect: false)
+
+           ])
+        
+        ,
+        
+        
+        Question(correctAnswer:
+
+                Answer(text: "Ukraine", isCorrect: true),
+
+                 incorrectAnswer: [
+
+                Answer(text: "China", isCorrect: false),
+
+                Answer(text: "Greece", isCorrect: false),
+
+                Answer(text: "Norway", isCorrect: false)
+
+           ])
+
+        ,
+        
+        
+        Question(correctAnswer:
+
+                Answer(text: "China", isCorrect: true),
+
+                 incorrectAnswer: [
+
+                Answer(text: "United States", isCorrect: false),
+
+                Answer(text: "Ukraine", isCorrect: false),
+
+                Answer(text: "Sweden", isCorrect: false)
+
+           ])
  
+        ,
+        
+        
+        Question(correctAnswer:
+
+                Answer(text: "India", isCorrect: true),
+
+                 incorrectAnswer: [
+
+                Answer(text: "Israel", isCorrect: false),
+
+                Answer(text: "South Africa", isCorrect: false),
+
+                Answer(text: "Saudi Arabia", isCorrect: false)
+
+           ])
+        
+        ,
+        
+        
+        Question(correctAnswer:
+
+                Answer(text: "Estonia", isCorrect: true),
+
+                 incorrectAnswer: [
+
+                Answer(text: "Austria", isCorrect: false),
+
+                Answer(text: "Astonia", isCorrect: false),
+
+                Answer(text: "Pakistan", isCorrect: false)
+
+           ])
+        
+        ,
+        
+        
+        Question(correctAnswer:
+
+                Answer(text: "Pakistan", isCorrect: true),
+
+                 incorrectAnswer: [
+
+                Answer(text: "Spain", isCorrect: false),
+
+                Answer(text: "France", isCorrect: false),
+
+                Answer(text: "India", isCorrect: false)
+
+           ])
+        
+        ,
+        
+        
+        Question(correctAnswer:
+
+                Answer(text: "Saudi Arabia", isCorrect: true),
+
+                 incorrectAnswer: [
+
+                Answer(text: "Israel", isCorrect: false),
+
+                Answer(text: "Peru", isCorrect: false),
+
+                Answer(text: "United States", isCorrect: false)
+
+           ])
+        
+        ,
+        
+        
+        Question(correctAnswer:
+
+                Answer(text: "Norway", isCorrect: true),
+
+                 incorrectAnswer: [
+
+                Answer(text: "Angola", isCorrect: false),
+
+                Answer(text: "China", isCorrect: false),
+
+                Answer(text: "England", isCorrect: false)
+
+           ])
+        
+        ,
+        
+        
+        Question(correctAnswer:
+
+                Answer(text: "United States", isCorrect: true),
+
+                 incorrectAnswer: [
+
+                Answer(text: "Estonia", isCorrect: false),
+
+                Answer(text: "Greece", isCorrect: false),
+
+                Answer(text: "Sweden", isCorrect: false)
+
+           ])
+        
+        ,
+        
+        
+        Question(correctAnswer:
+
+                Answer(text: "Peru", isCorrect: true),
+
+                 incorrectAnswer: [
+
+                Answer(text: "India", isCorrect: false),
+
+                Answer(text: "Italy", isCorrect: false),
+
+                Answer(text: "Ukraine", isCorrect: false)
+
+           ])
+        
+        ,
+        
+        
+        Question(correctAnswer:
+
+                Answer(text: "Israel", isCorrect: true),
+
+                 incorrectAnswer: [
+
+                Answer(text: "Angola", isCorrect: false),
+
+                Answer(text: "China", isCorrect: false),
+
+                Answer(text: "Italy", isCorrect: false)
+
+           ])
+        
+        ,
+        
+        
+        Question(correctAnswer:
+
+                Answer(text: "Italy", isCorrect: true),
+
+                 incorrectAnswer: [
+
+                Answer(text: "France", isCorrect: false),
+
+                Answer(text: "South Africa", isCorrect: false),
+
+                Answer(text: "Germany", isCorrect: false)
+
+           ])
+        
+        ,
+        
+        
+        Question(correctAnswer:
+
+                Answer(text: "South Africa", isCorrect: true),
+
+                 incorrectAnswer: [
+
+                Answer(text: "Saudi Arabia", isCorrect: false),
+
+                Answer(text: "Spain", isCorrect: false),
+
+                Answer(text: "Greece", isCorrect: false)
+
+           ])
+        
+        ,
+        
+        
+        Question(correctAnswer:
+
+                Answer(text: "Spain", isCorrect: true),
+
+                 incorrectAnswer: [
+
+                Answer(text: "United States", isCorrect: false),
+
+                Answer(text: "Angola", isCorrect: false),
+
+                Answer(text: "Estonia", isCorrect: false)
+
+           ])
+        
+        ,
+        
+        
+        Question(correctAnswer:
+
+                Answer(text: "Sweden", isCorrect: true),
+
+                 incorrectAnswer: [
+
+                Answer(text: "Peru", isCorrect: false),
+
+                Answer(text: "France", isCorrect: false),
+
+                Answer(text: "Germany", isCorrect: false)
+
+           ])
 
     ]
 
 }
+// 4Angola, 4Austria, 4China, 4England, 4Estonia, 4France, 4Germany, 4Greece, 4India, 4Israel, 4Italy, 4Norway, 4Pakistan, 4Peru, 4Saudi Arabia, 4South Africa, 4Spain, 4Sweden, 4Ukraine, 4United States //
